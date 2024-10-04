86c6b4ff7b2c0740746c5d2b856fa6960bae61a9
Jorge Matos <jisapjisap@gmail.com>
2024-10-04 16:03:52 +0200

    init

---
+++ src/sections/Header.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header>
+      <div>
+        <a>
+          <img 
+            src="/images/xora.svg" 
+            alt="logo" 
+            width={115} 
+            height={55}
+          />
+        </a>
+      </div>
+    </header>
+  )
+}
+
+export default Header
