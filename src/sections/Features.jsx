73325bb559449eeb1cbb21e200ca83bfe2f8d973
Jorge Matos <jisapjisap@gmail.com>
2024-10-05 19:52:32 +0200

    features

---
+++ src/sections/Features.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+
+const Features = () => {
+  return (
+    <section>
+      <Element name="features">
+        <div className='container'>
+          <div className='relative flex'>
+            {[{
+              icon: 'test',
+              caption: 'caption',
+              caption: 'caption',
+              caption: 'caption'
+            }]}
+          </div>
+        </div>
+      </Element>
+    </section>
+  )
+}
+
+export default Features
