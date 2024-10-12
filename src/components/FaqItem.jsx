0f1d9f7b977c930a1346eae13ee11a93a17b1959
Jorge Matos <jisapjisap@gmail.com>
2024-10-12 18:24:06 +0200

    faq section

--- src/components/FaqItem.jsx
+++ src/components/FaqItem.jsx
@@ -1,5 +1,7 @@
 import clsx from 'clsx'
 import { useState } from 'react';
+import SlideDown from 'react-slidedown';
+import 'react-slidedown/lib/slidedown.css';
 
 
 const FaqItem = ({ item, index }) => {
@@ -15,9 +17,10 @@ const FaqItem = ({ item, index }) => {
           setActiveId(activeId === item.id ? null : item.id);
         }}  
       >
+        {/* question */}
         <div className='flex-1'>
           <div className='small-compact mb-1.5 text-p3 max-lg:hidden'>
-            {index < 10 ? '0' : ''}{index}
+            {index < 10 ? '0' : ''}{index}.
           </div>
           <div className={
             clsx(
@@ -29,6 +32,7 @@ const FaqItem = ({ item, index }) => {
           </div>
         </div>
 
+        {/* button */}
         <div className={
           clsx(
             "faq-icon relative flex size-12 items-center justify-center rounded-full border-2 border-s2 shadow-400 transition-all duration-500 group-hover:border-s4",
@@ -37,8 +41,24 @@ const FaqItem = ({ item, index }) => {
         }>
           <div className='g4 size-11/12 rounded-full shadow-300'/>
         </div>
-
       </div>
+
+      <SlideDown>
+        {activeId === item.id && (
+          <div className='body-3 px-7 py-3.5'>
+            {item.answer}
+          </div>
+        )}
+      </SlideDown>
+
+      <div className={clsx(
+        'g5 absolute -bottom-7 -top-7  left-0 right-0 -z-1 rounded-3xl opacity-0 tansition-opacity duration-500',
+        active && 'opacity-100'
+      )}>
+        <div className='g4 absolute inset-0.5 -z-1 rounded-3xl'/>
+        <div className='absolute left-8 top-0 h-0.5 w-40 bg-p1'/>      
+      </div>
+
     </div>
   )
 }
