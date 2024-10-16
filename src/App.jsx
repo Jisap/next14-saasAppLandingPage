1f44eb8315296554c60a18d5998b23ef89d30ee4
Jorge Matos <jisapjisap@gmail.com>
2024-10-16 17:20:00 +0200

    footer

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Pricing from './sections/Pricing'
 import { Faq } from './sections/Faq'
 import Testimonials from './sections/Testimonials'
 import Donwload from './sections/Donwload'
+import Footer from './sections/Footer'
 
 const App = () => {
   return (
@@ -17,6 +18,7 @@ const App = () => {
       <Faq />
       <Testimonials />
       <Donwload />
+      <Footer />
     </main>
   )
 }
